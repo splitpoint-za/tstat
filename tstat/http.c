e2ba606cd96147774e76311f361ae540bac5ea7a
munafo <munafo@da00f6ee-164e-4028-97e9-5fc1dd4040cf>
2013-07-11 12:18:50 +0000

    Code clean-up and bugfixes for the HTTP module -MMM-

--- tstat/http.c
+++ tstat/http.c
@@ -21,25 +21,22 @@
 #include "tcpL7.h"
 #include <regex.h>
 
-#define get_u8(X,O)   (*(tt_uint8  *)(X + O))
-#define get_u16(X,O)  (*(tt_uint16 *)(X + O))
-#define get_u32(X,O)  (*(tt_uint32 *)(X + O))
-
-#define HTTP_BUFFER_SIZE 1520
+#define HTTP_SMALL_BUFFER_SIZE  200
+#define HTTP_LARGE_BUFFER_SIZE 1600
 
 char *http_patterns[12];
 regex_t http_re[12];
 regmatch_t re_res[3];
-char http_url[1600];
+char http_url[HTTP_LARGE_BUFFER_SIZE];
 char http_method[10];
-char http_host[200];
-char http_ua[200];
-char http_ctype[200];
-char http_clen[200];
-char http_referer[1600];
+char http_host[HTTP_SMALL_BUFFER_SIZE];
+char http_ua[HTTP_SMALL_BUFFER_SIZE];
+char http_ctype[HTTP_SMALL_BUFFER_SIZE];
+char http_clen[HTTP_SMALL_BUFFER_SIZE];
+char http_referer[HTTP_LARGE_BUFFER_SIZE];
 char http_response[5];
-char http_range[200];
-char http_server[200];
+char http_range[HTTP_SMALL_BUFFER_SIZE];
+char http_server[HTTP_SMALL_BUFFER_SIZE];
 
 extern FILE *fp_http_logc;
 
@@ -89,7 +86,7 @@ void http_flow_stat(struct ip *pip, void *pproto, int tproto, void *pdir,
   void *pdata; /*start of payload */
   int data_length, payload_len;
   tcb *tcp_stats;
-  char *last_payload_char;
+  char last_payload_char;
 
   tcphdr *ptcp;
   ptcp = (tcphdr *) hdr;
@@ -102,6 +99,14 @@ void http_flow_stat(struct ip *pip, void *pproto, int tproto, void *pdir,
 
   if (ptp == NULL)
   	  return;
+ 
+  /* 
+    The only scope of the plugin is to write the log. 
+    Skip any elaboration if http_log_complete is disabled.
+  */
+  
+  if (fp_http_logc == NULL || !(LOG_IS_ENABLED(LOG_HTTP_COMPLETE)))
+    return;
 
   /* Content of the old FindConType function */
 
@@ -144,8 +149,8 @@ void http_flow_stat(struct ip *pip, void *pproto, int tproto, void *pdir,
              msize = re_res[2].rm_eo-re_res[2].rm_so;
 
               memcpy(http_url,base+re_res[2].rm_so,
-               (msize<1599?msize:1599));
-               http_url[msize<1599?msize:1599]='\0';
+               (msize<(HTTP_LARGE_BUFFER_SIZE-1)?msize:(HTTP_LARGE_BUFFER_SIZE-1)));
+               http_url[msize<(HTTP_LARGE_BUFFER_SIZE-1)?msize:(HTTP_LARGE_BUFFER_SIZE-1)]='\0';
 	       
 	     base += re_res[2].rm_eo;
 	    }
@@ -160,8 +165,8 @@ void http_flow_stat(struct ip *pip, void *pproto, int tproto, void *pdir,
              msize = re_res[2].rm_eo-re_res[2].rm_so;
 
               memcpy(http_url,base+re_res[2].rm_so,
-               (msize<1599?msize:1599));
-               http_url[msize<1599?msize:1599]='\0';
+               (msize<(HTTP_LARGE_BUFFER_SIZE-1)?msize:(HTTP_LARGE_BUFFER_SIZE-1)));
+               http_url[msize<(HTTP_LARGE_BUFFER_SIZE-1)?msize:(HTTP_LARGE_BUFFER_SIZE-1)]='\0';
 	    }
           else
 	   {
@@ -174,8 +179,8 @@ void http_flow_stat(struct ip *pip, void *pproto, int tproto, void *pdir,
              int msize = re_res[1].rm_eo-re_res[1].rm_so;
 
               memcpy(http_host,base+re_res[1].rm_so,
-               (msize<199?msize:199));
-               http_host[msize<199?msize:199]='\0';
+               (msize<(HTTP_SMALL_BUFFER_SIZE-1)?msize:(HTTP_SMALL_BUFFER_SIZE-1)));
+               http_host[msize<(HTTP_SMALL_BUFFER_SIZE-1)?msize:(HTTP_SMALL_BUFFER_SIZE-1)]='\0';
 	    }
           else
 	   {
@@ -187,8 +192,8 @@ void http_flow_stat(struct ip *pip, void *pproto, int tproto, void *pdir,
              int msize = re_res[1].rm_eo-re_res[1].rm_so;
 
               memcpy(http_referer,base+re_res[1].rm_so,
-               (msize<1599?msize:1599));
-               http_referer[msize<1599?msize:1599]='\0';
+               (msize<(HTTP_LARGE_BUFFER_SIZE-1)?msize:(HTTP_LARGE_BUFFER_SIZE-1)));
+               http_referer[msize<(HTTP_LARGE_BUFFER_SIZE-1)?msize:(HTTP_LARGE_BUFFER_SIZE-1)]='\0';
 	    }
           else
 	   {
@@ -200,8 +205,8 @@ void http_flow_stat(struct ip *pip, void *pproto, int tproto, void *pdir,
              int msize = re_res[1].rm_eo-re_res[1].rm_so;
 
               memcpy(http_ua,base+re_res[1].rm_so,
-               (msize<199?msize:199));
-               http_ua[msize<199?msize:199]='\0';
+               (msize<(HTTP_SMALL_BUFFER_SIZE-1)?msize:(HTTP_SMALL_BUFFER_SIZE-1)));
+               http_ua[msize<(HTTP_SMALL_BUFFER_SIZE-1)?msize:(HTTP_SMALL_BUFFER_SIZE-1)]='\0';
 	    }
           else
 	   {
@@ -223,29 +228,18 @@ void http_flow_stat(struct ip *pip, void *pproto, int tproto, void *pdir,
 
             wfprintf (fp_http_logc,"\t%s\t%s\t%s",http_url,http_referer,http_ua);
 
-            wfprintf(fp_http_logc,"\n");
+            wfprintf (fp_http_logc,"\n");
 	  }
-
-          if (0)
-          {
-           int ii;
-           printf ("%s %s",HostName(ptp->addr_pair.a_address),
-                                    ServiceName(ptp->addr_pair.a_port));
-           printf (" %s %s ",HostName(ptp->addr_pair.b_address),
-                                    ServiceName(ptp->addr_pair.b_port));
-           //printf ("%d ",ptp->http_data);
-           for (ii=0;ii<data_length;ii++)
-            {
-              printf ("%c",isprint(*(char *)(pdata+ii))?*(char *)(pdata+ii):'.');
-            }
-           printf("\n");
-          }
-
 	}
        break;
      case HTTP:
        if (dir == S2C)
         { 
+          if ((*(char *)(pdata+4))!='/')
+	   {
+	     break;
+	   }
+
   	  last_payload_char =  *((char *)(pdata + data_length));
   	  *(char *)(pdata + data_length) = '\0';
 
@@ -264,8 +258,8 @@ void http_flow_stat(struct ip *pip, void *pproto, int tproto, void *pdir,
              int msize = re_res[1].rm_eo-re_res[1].rm_so;
 
               memcpy(http_ctype,base+re_res[1].rm_so,
-               (msize<199?msize:199));
-               http_ctype[msize<199?msize:199]='\0';
+               (msize<(HTTP_SMALL_BUFFER_SIZE-1)?msize:(HTTP_SMALL_BUFFER_SIZE-1)));
+               http_ctype[msize<(HTTP_SMALL_BUFFER_SIZE-1)?msize:(HTTP_SMALL_BUFFER_SIZE-1)]='\0';
 	    }
           else
 	   {
@@ -277,8 +271,8 @@ void http_flow_stat(struct ip *pip, void *pproto, int tproto, void *pdir,
              int msize = re_res[1].rm_eo-re_res[1].rm_so;
 
               memcpy(http_clen,base+re_res[1].rm_so,
-               (msize<199?msize:199));
-               http_clen[msize<199?msize:199]='\0';
+               (msize<(HTTP_SMALL_BUFFER_SIZE-1)?msize:(HTTP_SMALL_BUFFER_SIZE-1)));
+               http_clen[msize<(HTTP_SMALL_BUFFER_SIZE-1)?msize:(HTTP_SMALL_BUFFER_SIZE-1)]='\0';
 	    }
           else
 	   {
@@ -290,8 +284,8 @@ void http_flow_stat(struct ip *pip, void *pproto, int tproto, void *pdir,
              int msize = re_res[1].rm_eo-re_res[1].rm_so;
 
               memcpy(http_range,base+re_res[1].rm_so,
-               (msize<199?msize:199));
-               http_range[msize<199?msize:199]='\0';
+               (msize<(HTTP_SMALL_BUFFER_SIZE-1)?msize:(HTTP_SMALL_BUFFER_SIZE-1)));
+               http_range[msize<(HTTP_SMALL_BUFFER_SIZE-1)?msize:(HTTP_SMALL_BUFFER_SIZE-1)]='\0';
 	    }
           else
 	   {
@@ -303,8 +297,8 @@ void http_flow_stat(struct ip *pip, void *pproto, int tproto, void *pdir,
              int msize = re_res[1].rm_eo-re_res[1].rm_so;
 
               memcpy(http_server,base+re_res[1].rm_so,
-               (msize<199?msize:199));
-               http_server[msize<199?msize:199]='\0';
+               (msize<(HTTP_SMALL_BUFFER_SIZE-1)?msize:(HTTP_SMALL_BUFFER_SIZE-1)));
+               http_server[msize<(HTTP_SMALL_BUFFER_SIZE-1)?msize:(HTTP_SMALL_BUFFER_SIZE-1)]='\0';
 	    }
           else
 	   {
@@ -316,8 +310,8 @@ void http_flow_stat(struct ip *pip, void *pproto, int tproto, void *pdir,
              int msize = re_res[1].rm_eo-re_res[1].rm_so;
 
               memcpy(http_url,base+re_res[1].rm_so,
-               (msize<1599?msize:1599));
-               http_url[msize<1599?msize:1599]='\0';
+               (msize<(HTTP_LARGE_BUFFER_SIZE-1)?msize:(HTTP_LARGE_BUFFER_SIZE-1)));
+               http_url[msize<(HTTP_LARGE_BUFFER_SIZE-1)?msize:(HTTP_LARGE_BUFFER_SIZE-1)]='\0';
 	    }
           else
 	   {
@@ -337,35 +331,13 @@ void http_flow_stat(struct ip *pip, void *pproto, int tproto, void *pdir,
             wfprintf (fp_http_logc,"\t%s\t%s\t%s\t%s","HTTP",http_response,http_clen,http_ctype);
             wfprintf (fp_http_logc,"\t%s\t%s\t%s",http_server,http_range,http_url);
 
-            wfprintf(fp_http_logc,"\n");
+            wfprintf (fp_http_logc,"\n");
 	  }
-
-          if (0)
-          {
-           int ii;
-           printf ("%s %s",HostName(ptp->addr_pair.a_address),
-                                    ServiceName(ptp->addr_pair.a_port));
-           printf (" %s %s ",HostName(ptp->addr_pair.b_address),
-                                    ServiceName(ptp->addr_pair.b_port));
-           //printf ("%d ",ptp->http_data);
-           for (ii=0;ii<data_length;ii++)
-            {
-              printf ("%c",isprint(*(char *)(pdata+ii))?*(char *)(pdata+ii):'.');
-            }
-           printf("\n");
-          }
-
 	}
        break;
      default:
-       // if (ptp->packets > MAX_UNKNOWN_PACKETS)
-       //   ptp->streaming.state = IGNORE_FURTHER_PACKETS;
        break;
    }
-
-  // if (ptp->packets > MAX_PACKETS_CON)
-  //  ptp->streaming.state = IGNORE_FURTHER_PACKETS;
-
 
   return;
 }
